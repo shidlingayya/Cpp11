c772f5b3a9d49d9338863133b23425d1f4cde827
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2025-12-06 17:05:14 +0530

    Add files via upload

---
+++ final_keyword_inheritance.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+
+class Google
+{
+public:
+	virtual void display()
+	{
+		std::cout << "Welcome to Google: " << std::endl;
+	}
+};
+
+class GooglePlus final :public Google
+{
+public:
+	void display()
+	{
+		std::cout << "Inside GooglePlus" << std::endl;
+	}
+};
+
+class Facebook :public GooglePlus
+{
+public:
+	void display() final
+	{
+		std::cout << "Facebook" << std::endl;
+	}
+};
+
+int main()
+{
+	return 0;
+}
