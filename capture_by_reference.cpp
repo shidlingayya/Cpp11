cc2c4a1f28998ee560b3cd53ad32bbefb6bd7b6d
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2025-12-06 06:34:01 +0530

    Add files via upload

---
+++ capture_by_reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	std::vector<int> myList{ 10, 20, 30, 40, 50 };
+	int argument = 0;
+	std::cout << "Get value of argument: ";
+	std::cin >> argument;
+	auto found = std::find_if(myList.begin(), myList.end(), [&argument](int x) {return x == argument; });
+	if (found != myList.end())
+	{
+		std::cout << "Element found in list: " << std::endl;
+	}
+	else
+	{
+		std::cout << "Element not found in list " << std::endl;
+	}
+}
